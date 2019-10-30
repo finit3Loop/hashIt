0f6d4ec82fe9206cb0beda138f0a3fc4b3f5d468
finiteLoop <12jpiper@gmail.com>
2019-10-30 01:19:14 -0500

    testing with Hasher 2

--- src/main/java/com/aws/codestar/projecttemplates/controller/Hasher2Controller.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/Hasher2Controller.java
@@ -9,20 +9,13 @@ import org.springframework.web.servlet.ModelAndView;
  * Hashing controller Spring MVC controller that handles all GET requests.
  */
 @Controller
-@RequestMapping("/Hasher2")
+
 public class Hasher2Controller {
 
-    private final String siteName2;
-
-    public Hasher2Controller(final String siteName2) {
-        this.siteName2 = siteName2;
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView helloWorld() {
-        ModelAndView mav = new ModelAndView("Hasher2");
-        mav.addObject("siteName2", this.siteName2);
-        return mav;
+	@RequestMapping("/Hasher2")
+	public String HasherController2() {
+		System.out.println("Hasher 2 controller");
+		return "Hasher2.jsp";
     }
 
 }
