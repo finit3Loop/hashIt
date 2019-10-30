fdb4fdfc96c7fc7c5a2defbac4e9a378a8afcdc9
finiteLoop <12jpiper@gmail.com>
2019-10-30 01:22:28 -0500

    Test to confirm use of Hasher 2 controller.

--- src/main/java/com/aws/codestar/projecttemplates/controller/Hasher2Controller.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/Hasher2Controller.java
@@ -3,6 +3,7 @@ package com.aws.codestar.projecttemplates.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class Hasher2Controller {
 
 	@RequestMapping("/Hasher2")
+	@ResponseBody
 	public String HasherController2() {
 		System.out.println("Hasher 2 controller");
 		return "Hasher2.jsp";
